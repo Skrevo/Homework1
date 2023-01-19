46b35368a0c0e974b62f393b819ad532c482794b
Дмитрий <Skrevo@outlook.com>
2023-01-19 20:22:41 +0200

    function y main test

--- src/test/java/main/MainTest.java
+++ src/test/java/main/MainTest.java
@@ -1,9 +1,12 @@
 package main;
 
+import org.assertj.core.data.Percentage;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class MainTest {
 
@@ -11,7 +14,9 @@ class MainTest {
     void setUp() {
     }
 
-    @Test
-    void testFunction() {
+    @ParameterizedTest
+    @CsvSource({"1.7, 16.4963", "1.5, 3.9574", "2.0, 1.0755"})
+    void testFunction(double x, double expectedY) {
+        assertThat(Main.function(x)).isCloseTo(expectedY, Percentage.withPercentage(0.01));
     }
 }
