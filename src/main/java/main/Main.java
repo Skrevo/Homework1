31ce45b41fe688b250cb9ddd8a61975b89395532
Дмитрий <Skrevo@outlook.com>
2023-01-19 20:39:44 +0200

    countNumberOfSteps main

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -7,6 +7,8 @@ public class Main {
         
         double y = Main.function(2);
         System.out.println(y);
+        int numberOfSteps = Main.countNumberOfSteps(0.8,2.0,0.005);
+        System.out.println(numberOfSteps);
     }
 
     private static final double A = 1.5;
@@ -18,5 +20,16 @@ public class Main {
             return A * Math.pow(x,3) + 7 * Math.sqrt(x);
         }
         return Math.log10(x + 7 * Math.sqrt(x));    //if (x > 1.7)
+    }
+
+    public static int countNumberOfSteps(double borderDown, double borderUp, double delta) {
+
+        double currentY = borderDown;
+        int count = 0;
+        while (currentY <= borderUp) {
+            count++;
+            currentY+= delta;
+        }
+        return count;
     }
 }
